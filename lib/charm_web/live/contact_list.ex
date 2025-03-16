73e028e3ece8d4b293f62a57a51e44649f0648ca
Guillaume Breton <breton.gy@gmail.com>
2025-03-16 18:09:14 +0100

    Use the app layout

--- lib/charm_web/live/contact_list.ex
+++ lib/charm_web/live/contact_list.ex
@@ -2,13 +2,13 @@ defmodule CharmWeb.ContactList do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket, layout: {CharmWeb.Layouts, :app}}
   end
 
   def render(assigns) do
     ~H"""
     <div>
-      Contact List Content
+      Contact List Content test
     </div>
     """
   end
