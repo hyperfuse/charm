45a419a8715122669a99ddfd83ef439b4fdebb6b
Guillaume Breton <breton.gy@gmail.com>
2025-03-16 18:29:08 +0100

    Order the list by name

--- lib/charm/contacts.ex
+++ lib/charm/contacts.ex
@@ -1,12 +1,17 @@
 defmodule Charm.Contacts do
   alias Charm.Repo
   alias Charm.Contacts.Contact
+  import Ecto.Query, only: [from: 2]
 
   @doc """
   List all the contacts in the database.
   """
   @spec list_contacts :: list(Contact.t())
   def list_contacts do
-    Repo.all(Contact)
+    query =
+      from c in Charm.Contacts.Contact,
+        order_by: [asc: c.last_name, asc: c.first_name]
+
+    Repo.all(query)
   end
 end
